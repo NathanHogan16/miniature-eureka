b12b40096c46bc5adfa3806e225a8eb56534c5cc
Nate Hogan <natek.hogan@gmail.com>
2021-04-05 20:16:01 -0600

    finish project

--- db/store.js
+++ db/store.js
@@ -1,11 +1,11 @@
-const util = require("util");
+//const util = require("util");
 const fs = require("fs");
 
 // This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
-const uuidv1 = require('uuid/v1');
+//const uuidv1 = require('uuid/v1');
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+//const readFileAsync = util.promisify(fs.readFile);
+//const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
   read() {
